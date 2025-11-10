5e317c35eac9a9627ff8787243cb151634e6b3af
ishu17077 <ishu17077@users.noreply.github.com>
2025-11-10 18:37:09 +0530

    Fix: Display HomeScreen as default

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        "/": (context) => CompositionRoot.composeManageStorageUi(),
+        "/": (context) => CompositionRoot.start(),
         "/manage-storage": (_) => CompositionRoot.composeManageStorageUi(),
       },
     );
